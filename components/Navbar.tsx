e623a846e00739247a18192a6355f57d7c67b290
yuvaraj337 <rayalarhonda777@gmail.com>
2026-08-20 06:55:02 +0530

    Initial commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -198,7 +198,7 @@ export default function Navbar() {
             {/* MOBILE RIGHT */}
             {/* ========================= */}
 
-            <div className="lg:hidden flex items-center gap-4">
+            <div className="lg:hidden flex items-center gap-7">
 
   {/* STATUS — LEFT OF CART */}
   <RestaurantStatus />
